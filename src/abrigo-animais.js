2e37abfe468130ab4fd48a3b07789c50f4b2a220
Camila Feldkircher Antunes <99697225+camilafeldantunes@users.noreply.github.com>
2025-09-08 20:21:28 -0300

    criando verifica brinquedos

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -10,19 +10,69 @@ class AbrigoAnimais {
       Bebe: {tipo: "cão", brinquedo:["LASER", "RATO", "BOLA"]},
       Loco: {tipo: "jabuti", brinquedo:["SKATE", "RATO"]},
     }
+    this.brinquedo = ["RATO", "BOLA", "LASER", "CAIXA", "NOVELO", "SKATE"];
   }
 
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-    var arrayBrinquedos = brinquedosPessoa1.split(",");
-    var arrayBrinquedos2 = brinquedosPessoa2.split(",");
-    var arrayAnimais = ordemAnimais.split(",");
+    try{
+      var arrayBrinquedosPessoa1 = brinquedosPessoa1.split(",");
+      var arrayBrinquedosPessoa2 = brinquedosPessoa2.split(",");
+      var arrayAnimais = ordemAnimais.split(",");
+      var resultado = [];
 
+
+      for(var nome of arrayAnimais){
+        var animal = this.animais[nome];
+          if(!animal){
+            return { erro: "Animal inválido"};
+          }
+      }
+
+      var brinquedoAnimal = animal.brinquedo;
+
+      var pessoaUm = verificaBrinquedos(brinquedoAnimal, arrayBrinquedosPessoa1);
+      var pessoaDois = verificaBrinquedos(brinquedoAnimal, arrayBrinquedosPessoa2);
+
+     if (pessoaUm && !pessoaDois) {
+        resultado.push(nome + " - pessoa 1");
+      } else if (!pessoaUm && pessoaDois) {
+        resultado.push(nome + " - pessoa 2");
+      } else {
+        resultado.push(nome + " - abrigo");
+      }
+
+
+
+      return { lista: resultado.sort()};
+
+    } catch(e){
+
+        return { erro: "Animal Inválido"};
+    }
+    
 
   }
 
 }
 
+    verificaBrinquedos(listaAnimalBrinquedo, listaPessoaBrinquedo){
+      var i = 0;
+      for(var brinquedo of listaPessoaBrinquedo){
+        if(brinquedo == listaAnimalBrinquedo[i]){
+          i++;
+      }
+      if (i == listaAnimal.length){
+        return true;
+      }
+      return i == listaAnimal.length;
+        
+    }
+  }
+  
+  
+
 
 
 export { AbrigoAnimais as AbrigoAnimais };
+
